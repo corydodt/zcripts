04096249f37102b9177687d150592898d356d69b
cory <121705+corydodt@users.noreply.github.com>
2024-04-29 22:02:18 +0000

    building some environment config for zcripts

--- src/zcripts/zcripts_cli.py
+++ src/zcripts/zcripts_cli.py
@@ -3,24 +3,50 @@ zcripts
 """
 
 import argparse
-import glob
+from copy import deepcopy
 import os
+from pathlib import Path
 import sys
 
-DEFAULT_ZCRIPTS_PATH = "/zcriptsinit/host.d"
+import tomlkit
 
-def find_script(where, hostname):
-    match_exact = glob.glob(f"{where}/{hostname}")
-    match_prefix = glob.glob(f"{where}/{hostname}.*")
-    match_zcripts_exact = glob.glob(f"{where}/zcripts.{hostname}")
-    match_zcripts_prefix = glob.glob(f"{where}/zcripts.{hostname}.*")
+
+DEFAULT_ZCRIPTS_HOME = Path("/zcriptsinit")
+
+
+def find_script(where: Path, hostname: str):
+    hostd = where / "host.d"
+    match_exact = hostd.glob(f"{hostname}")
+    match_prefix = hostd.glob(f"{hostname}.*")
+    match_zcripts_exact = hostd.glob(f"zcripts.{hostname}")
+    match_zcripts_prefix = hostd.glob(f"zcripts.{hostname}.*")
     for tries in match_exact, match_prefix, match_zcripts_exact, match_zcripts_prefix:
-        if len(tries) > 0:
-            return tries[0]
+        for item in tries:
+            return item
     
     # if we reached this point, we didn't find anything. return the exact
-    # path we tried first and let upstream report file not found.
-    return f"{where}/{hostname}"
+    # path we tried first and let upstream report File Not Found.
+    return hostd / hostname
+
+
+def read_defaults(home: Path):
+    return read_toml(home / "defaults.toml")
+
+
+def read_toml(filepath: Path):
+    if not filepath.exists():
+        return {}
+
+    with filepath.open("rb") as f:
+        data = tomlkit.load(f)
+
+    return data
+
+
+def update_config(config, toml_path: Path):
+    clone = deepcopy(config)
+    clone.update(read_toml(toml_path))
+    return clone
 
 
 def main():
@@ -28,13 +54,25 @@ def main():
     parser.description = __doc__
     parser.formatter_class = argparse.RawDescriptionHelpFormatter
     parser.add_argument("--first-boot", nargs=1, metavar="HOSTNAME")
-    parser.add_argument("--zcripts-path", nargs=1, default=DEFAULT_ZCRIPTS_PATH)
+    parser.add_argument("--zcripts-home", nargs=1, default=DEFAULT_ZCRIPTS_HOME)
+    parser.add_argument("--dump-config-only", action="store_true")
     ns = parser.parse_args()
 
+    defaults = read_defaults(ns.zcripts_home)
+
     if ns.first_boot:
-        script_path = find_script(ns.zcripts_path, ns.first_boot[0])
+        script_path = find_script(ns.zcripts_home, ns.first_boot[0])
+        overloads = update_config(defaults, script_path.with_suffix(script_path.suffix + ".toml"))
+
+        if ns.dump_config_only:
+            print(tomlkit.dumps(overloads))
+            return 0
+
+        new_env = deepcopy(os.environ)
+        new_env.update(overloads["environment"])
+
         try:
-            os.execl(script_path, script_path)
+            os.execle(script_path, script_path, new_env)
         except OSError as e:
             raise parser.error(f"{script_path}: {e}")
 
